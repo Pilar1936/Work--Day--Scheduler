68da5dea3fa4d23d5dbe4a0f04bf0134a78601ab
Pilar1936 <tesoro4me@hotmail.com>
2024-03-03 10:37:58 -0600

    comments

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+// Función para mostrar la hora actual
+// Function to display the current time
 var timeDisplayDate = $('#currentDay');
 
 function displayTime(){
   var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
   timeDisplayDate.text(rightNow);
 }
-
+// Evento click en el botón de guardar
+// Click event on the save button
 $(".saveBtn").on('click',function(){
   console.log($(this))
   console.log($(this).siblings(".description").val())
@@ -13,7 +16,8 @@ $(".saveBtn").on('click',function(){
   localStorage.setItem(blockId,blockText);
 
 })
-
+// Actualizar estilos de bloques de tiempo
+//// Update time block styles 
 var currentHour = dayjs().hour();
 $(".time-block").each(function() {
   var blockHour = parseInt($(this).attr("id").split("-")[1]);
@@ -25,10 +29,12 @@ $(".time-block").each(function() {
     $(this).removeClass('past present').addClass("future");
   }
 });
+// Cargar texto guardado en localStorage
+// Load text saved in localStorage
 
 $(".time-block").each(function() {
   var blockId = $(this).attr('id');
-  var saveText = localStorage.getItem(blockId); // Corregido aquí, cambió de localStorage.getId() a localStorage.getItem()
+  var saveText = localStorage.getItem(blockId); 
   if (saveText) {
     $(this).find(".description").val(saveText);
   }
